eacec19c4f5adefa2589ac5fde669737d9642150
Pieter Beulque <pieterbeulque@gmail.com>
2017-01-26 11:52:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# dynamic-properties
+
+An easy way to handle repaint triggering properties in your JS, with value caching & cache invalidation on resize (throttled).
+
+Documentation is still being written. Ask [@pieterbeulque](mailto:pieter@mrhenry.be).
---
+++ index.js
@@ -0,0 +1,56 @@
+export default class DynamicProperties {
+
+	constructor(properties = {}, settings = { throttle: 64 }) {
+		let cached = {};
+
+		const addProperty = (key, getter, element = null) => {
+			if (typeof this[key] !== 'undefined') {
+				console.warn(key, 'is already defined in this property list');
+				return false;
+			}
+
+			Object.defineProperty(this, key, {
+				enumerable: true,
+				configurable: true,
+				get: () => {
+					if (typeof cached[key] === 'undefined') {
+						if (element) {
+							cached[key] = getter.call(element);
+						} else {
+							cached[key] = getter();
+						}
+					}
+
+					return cached[key];
+				}
+			});
+
+			return this;
+		};
+
+		for (let key in properties) {
+			const getter = properties[key];
+
+			if (typeof getter === 'function') {
+				addProperty(key, getter);
+			} else if (typeof getter !== 'string' && getter.length) {
+				// Lazy array check
+				addProperty(key, getter[0], getter[1] || window);
+			}
+		}
+
+		// Reset cache on resize
+		let resizeTimeout;
+		window.addEventListener('resize', e => {
+			if (resizeTimeout) {
+				clearTimeout(resizeTimeout);
+			}
+
+			resizeTimeout = setTimeout(() => {
+				cached = {};
+				resizeTimeout = undefined;
+			}, settings.throttle);
+		});
+	}
+
+}
