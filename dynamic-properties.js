e96608ed7fab0eb4f09481cb11b5c06954e52234
Pieter Beulque <pieterbeulque@gmail.com>
2020-05-11 10:37:28 +0200

    Take over project config

--- dynamic-properties.js
+++ dynamic-properties.js
@@ -1,68 +1,68 @@
 export default function dynamicProperties(properties = {}, settings = { throttle: 64 }) {
-	const EMITTER = document.createDocumentFragment();
-	let cached = {};
+  const EMITTER = document.createDocumentFragment();
+  let cached = {};
 
-	const addEventListener = (...args) => {
-		EMITTER.addEventListener(...args);
-	};
+  const addEventListener = (...args) => {
+    EMITTER.addEventListener(...args);
+  };
 
-	const removeEventListener = (...args) => {
-		EMITTER.removeEventListener(...args);
-	};
+  const removeEventListener = (...args) => {
+    EMITTER.removeEventListener(...args);
+  };
 
-	const dispatchEvent = (...args) => {
-		EMITTER.dispatchEvent(...args);
-	};
+  const dispatchEvent = (...args) => {
+    EMITTER.dispatchEvent(...args);
+  };
 
-	const store = { addEventListener, removeEventListener, dispatchEvent };
+  const store = { addEventListener, removeEventListener, dispatchEvent };
 
-	const addProperty = (key, getter, element = null) => {
-		if (typeof store[key] !== 'undefined') {
-			console.warn(key, 'is already defined in this property list');
-			return false;
-		}
+  const addProperty = (key, getter, element = null) => {
+    if (typeof store[key] !== 'undefined') {
+      console.warn(key, 'is already defined in this property list');
+      return false;
+    }
 
-		Object.defineProperty(store, key, {
-			enumerable: true,
-			configurable: false,
-			get: () => {
-				if (typeof cached[key] !== 'undefined') {
-					return cached[key];
-				}
+    Object.defineProperty(store, key, {
+      enumerable: true,
+      configurable: false,
+      get: () => {
+        if (typeof cached[key] !== 'undefined') {
+          return cached[key];
+        }
 
-				const value = (element) ? getter.call(element) : getter();
-				cached[key] = value;
-				return cached[key];
-			},
-		});
+        const value = (element) ? getter.call(element) : getter();
+        cached[key] = value;
+        return cached[key];
+      },
+    });
 
-		return store;
-	};
+    return store;
+  };
 
-	Object.entries(properties).forEach(([key, getter]) => {
-		if (typeof getter === 'function') {
-			addProperty(key, getter);
-		} else if (typeof getter !== 'string' && getter.length) {
-			// Lazy array check
-			addProperty(key, getter[0], getter[1] || window);
-		}
-	});
+  Object.entries(properties).forEach(([key, getter]) => {
+    if (typeof getter === 'function') {
+      addProperty(key, getter);
+    } else if (typeof getter !== 'string' && getter.length) {
+      // Lazy array check
+      addProperty(key, getter[0], getter[1] || window);
+    }
+  });
 
-	// Reset cache on resize
-	let resizeTimeout;
-	window.addEventListener('resize', () => {
-		if (resizeTimeout) {
-			clearTimeout(resizeTimeout);
-		}
+  // Reset cache on resize
+  let resizeTimeout;
+  window.addEventListener('resize', () => {
+    if (resizeTimeout) {
+      clearTimeout(resizeTimeout);
+    }
 
-		resizeTimeout = setTimeout(() => {
-			cached = {};
-			resizeTimeout = undefined;
+    resizeTimeout = setTimeout(() => {
+      cached = {};
+      resizeTimeout = undefined;
 
-			const change = new Event('change');
-			store.dispatchEvent(change);
-		}, settings.throttle);
-	});
+      const change = new Event('change');
+      store.dispatchEvent(change);
+    }, settings.throttle);
+  });
 
-	return Object.freeze(store);
+  return Object.freeze(store);
 }
